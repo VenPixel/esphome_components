8b78b4cb810d75269374cb6b9fc5672cac3aebe0
VenPixel <venpixel@gmail.com>
2025-07-06 15:37:02 -0400

    Loading fix

--- components/monk_plant_monitor/sensor.py
+++ components/monk_plant_monitor/sensor.py
@@ -55,8 +55,6 @@ async def to_code(config):
     # await cg.register_component(var, config)
     # await uart.register_uart_device(var, config)
     var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    await uart.register_uart_device(var, config)
 
     # cg.add(var.set_soil_sensor(config[CONF_SOIL]))
 
@@ -67,3 +65,6 @@ async def to_code(config):
     cg.add(var.set_soil_sensor(soil))
     # cg.add(var.set_temp_sensor(temp))
     # cg.add(var.set_humidity_sensor(hum))
+
+    await cg.register_component(var, config)
+    await uart.register_uart_device(var, config)
